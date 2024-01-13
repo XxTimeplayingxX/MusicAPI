85c440a804fe2cc885d3a32786dcea981dd9a9d5
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-01-12 20:42:50 -0500

    Agregar archivos de proyecto.

---
+++ MusicAPI/Controllers/SongsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Models;
+
+namespace MusicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> _songs = new List<Song>
+        {
+            new Song
+            {
+                ID = 1,
+                Title = "Almost Home",
+                Language = "English"
+            },
+            new Song
+            {
+                ID = 2,
+                Title = "Yeah",
+                Language = "English"
+            },
+            new Song
+            {
+                ID = 3,
+                Title = "Entre Nosotros",
+                Language = "Español"
+            }
+
+
+        };
+
+        [HttpGet]//Mediante este controlador podemos hacer la petición
+        public IEnumerable<Song> GetAllSongs()
+        {
+            return _songs;
+        }
+    }
+}
