f6667f8bd3f50a1a0b9e93e117f9ca846b3f3848
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-01-19 19:20:13 -0500

    CAMBIO 1

--- MusicAPI/Controllers/SongsController.cs
+++ MusicAPI/Controllers/SongsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MusicAPI.Models;
+using System.Runtime.CompilerServices;
 
 namespace MusicAPI.Controllers
 {
@@ -37,5 +38,51 @@ namespace MusicAPI.Controllers
         {
             return _songs;
         }
+        //Get api/songscontroller/0
+        [HttpGet("{ID}")]
+        //Al consultar por el ID, nos devuelva una canción en específico
+        public Song GetSongByID(int ID)
+        {
+            return _songs.Find(song => song.ID == ID);
+        }
+        [HttpPost]
+        //Vamos a usar algo llamado ActionResulta
+        public IActionResult SaveOneSong([FromBody] Song newsong)
+        {
+            _songs.Add(newsong);
+            return Ok();
+        }
+        [HttpPut("{ID}/{newTitle}")]
+        public IActionResult UpdateSong(int ID, string newTitle)
+        {
+            var song = _songs.Find(song => song.ID == ID);
+            song.Title = newTitle;
+
+            return Ok();
+        }
+
+        //Put api/song/0
+        [HttpPut("{ID}")]
+        public IActionResult UpdateSong(int ID,[FromBody] Song updatedSong)
+        {
+            var song = _songs.Find(song => song.ID == ID);
+
+            song.Title = updatedSong.Title;
+            song.Language = updatedSong.Language; 
+
+            return Ok();
+        }
+        //Debemos de hacer el método eliminar
+
+
+        [HttpDelete("{ID}")]
+        public ActionResult DeleteSong(int ID)
+        {
+            var song = _songs.Find(song => song.ID == ID);
+            _songs.Remove(song);
+
+            return Ok();
+        }
+
     }
 }
