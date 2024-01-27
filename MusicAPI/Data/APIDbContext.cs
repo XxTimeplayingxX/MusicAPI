be7c0be9864748f11878ab278f03ab1901ac2c37
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-01-26 19:42:58 -0500

    v3.1

---
+++ MusicAPI/Data/APIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAPI.Models;
+
+namespace MusicAPI.Data
+{
+    public class APIDbContext : DbContext
+    {
+        //Necesita opciones un constructor donde agregamos las opciones
+        public APIDbContext(DbContextOptions<APIDbContext> options) 
+            : base (options)
+        {
+            
+        }
+        public DbSet<Song> Songs{ get; set; }
+    }
+}
